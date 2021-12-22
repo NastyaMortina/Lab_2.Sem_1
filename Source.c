b705c5a974aea5d190ce56b2c35952d8d85644be
NastyaMortina <90023831+NastyaMortina@users.noreply.github.com>
2021-12-22 15:18:04 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,50 +1,152 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h> 
 #include <stdio.h>
-#include <locale.h> 
+#include <locale.h>
+#include <time.h>
+
+void bubble(unsigned char* mas, int n)
+{
+    int i = 0;
+    int j = 0;
+    unsigned char temp = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (mas[j] > mas[j + 1])
+            {
+                temp = mas[j];
+                mas[j] = mas[j + 1];
+                mas[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void quick(unsigned char* mas, int n)
+{
+    int i = 0;
+    int j = n - 1;
+    int mid = mas[n / 2];
+
+    do
+    {
+        while (mas[i] < mid) i++;
+        while (mas[j] > mid) j--;
+
+        if (i <= j)
+        {
+            int tmp = mas[i];
+            mas[i] = mas[j];
+            mas[j] = tmp;
+            i++;
+            j--;
+        }
+    } while (i <= j);
+
+    if (j > 0) quick(mas, j + 1);
+    if (i < n) quick(&mas[i], n - i);
+}
+
+void insertion(unsigned char* mas, int n)
+{
+    unsigned char temp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        temp = mas[i];
+        int j = i;
+        while ((j > 0) && (mas[j - 1] > temp))
+        {
+            mas[j] = mas[j - 1];
+            j--;
+        }
+        mas[j] = temp;
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    int i = 0;
     int n = 0;
-    int Min = 0;
-    int Max = 0;
-
+    int i = 0;
     unsigned char* mas;
-
-    printf("Количество чисел: ");
-    scanf("%d", &n);
-    printf("Min граница: ");
-    scanf("%d", &Min);
-    printf("Max граница: ");
-    scanf("%d", &Max);
-    printf("\n");
-
-    if ((Max <= Min) || (Max > 255) || (Min > 255) || (Min <= 0) || (n <= 0) || (Max <= 0))
-    {
-        printf("Ошибка\n");
-        return 0;
-    }
-
-    mas = (unsigned char*)malloc(n * sizeof(unsigned char));
-
-    for (i = 0; i < n; i++)
-    {
-        mas[i] = (((((double)rand()) / RAND_MAX) * (Max - Min)) + Min);
-    }
+    int choice = 0;
 
     FILE* file;
-    file = fopen("textFile.txt", "w+");
-    fprintf(file, "%d\n", n);
+    file = fopen("C:/Users/Nastya/source/repos/Project.Laboratornaya2/Project.Laboratornaya2/textFile.txt", "r+");
 
-    for (i = 0; i < n; i++)
+    fscanf_s(file, "%d", &n);
+
+    mas = (unsigned char*)malloc(n * sizeof(unsigned int));
+
+    for (i = 1; i < n + 1; i++)
     {
-        fprintf(file, "%d\n", mas[i]);
+        fscanf_s(file, "%u\n", &mas[i - 1]);
+    }
+
+
+    printf("[1] Печать\n");
+    printf("[2] Сортировка пузырёк\n");
+    printf("[3] Быстрая сортировка\n");
+    printf("[4] Сортировка вставкой\n");
+    printf("[5] Выход\n\n");
+    printf("Выбор: ");
+    scanf_s("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%u\n", mas[i]);
+        }
+        break;
+    case 2:
+        printf("\n");
+        unsigned int start1 = clock();
+        bubble(mas, n);
+        for (i = 0; i < n; i++)
+        {
+            printf("%u\n", mas[i]);
+        }
+        unsigned int end1 = clock();
+        unsigned int search1 = end1 - start1;
+        printf("\nВремя: %u мс\n", search1);
+        break;
+    case 3:
+        printf("\n");
+        unsigned int start2 = clock();
+        quick(mas, n);
+        for (i = 0; i < n; i++)
+        {
+            printf("%u\n", mas[i]);
+        }
+        unsigned int end2 = clock();
+        unsigned int search2 = end2 - start2;
+        printf("\n\nВремя: %u мс\n", search2);
+        break;
+    case 4:
+        printf("\n");
+        unsigned int start3 = clock();
+        insertion(mas, n);
+        for (i = 0; i < n; i++)
+        {
+            printf("%u\n", mas[i]);
+        }
+        unsigned int end3 = clock();
+        unsigned int search3 = end3 - start3;
+        printf("\n\nВремя: %u мс\n", search3);
+        break;
+    case 5:
+        free(mas);
+        fclose(file);
+        return 0;
     }
 
     free(mas);
     fclose(file);
     return 0;
 }
+
